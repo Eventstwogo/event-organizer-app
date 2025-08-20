1343e093d09495d9e0f052e7077bd3aeda320b16
KDasaradh <kdasaradha525@gmail.com>
2025-08-20 16:41:58 +0530

    chore: updated coupon values dropdown

--- src/app/(admin)/Coupons/page.tsx
+++ src/app/(admin)/Coupons/page.tsx
@@ -66,7 +66,7 @@ const CouponsPage = () => {
   // Form state
   const [formData, setFormData] = useState({
     name: "",
-    percentage: "10",
+    percentage: "5",
     no_of_tickets: "",
   });
 
@@ -429,15 +429,25 @@ const CouponsPage = () => {
                   }
                   className="col-span-3 p-2 border rounded-md"
                 >
+                  <option value="5">5%</option>
                   <option value="10">10%</option>
+                  <option value="15">15%</option>
                   <option value="20">20%</option>
+                  <option value="25">25%</option>
                   <option value="30">30%</option>
+                  <option value="35">35%</option>
                   <option value="40">40%</option>
+                  <option value="45">45%</option>
                   <option value="50">50%</option>
+                  <option value="55">55%</option>
                   <option value="60">60%</option>
+                  <option value="65">65%</option>
                   <option value="70">70%</option>
+                  <option value="75">75%</option>
                   <option value="80">80%</option>
+                  <option value="85">85%</option>
                   <option value="90">90%</option>
+                  <option value="95">95%</option>
                   <option value="100">100%</option>
                 </select>
               </div>
